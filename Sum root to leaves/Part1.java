bfd4732633ecb5827c5639be0a8b90f00f7afa3f
Anurag Agrawal <anuragagrawal4998@gmail.com>
2025-01-12 18:11:27 +0530

    add comments

--- Sum root to leaves/Part1.java
+++ Sum root to leaves/Part1.java
@@ -1,3 +1,14 @@
+/*
+We are given a binary tree and each path from root to a leaf represents a number, we have to find the sum of all such numbers and return it.
+
+We can travel in the tree in a recursive fashion, and as soon as we reach a leaf node we should return the number formed at that point.
+
+As we travel from root to the leaf nodes, we can pass the value of root at each point, to generate our numbers, just multiply the number with
+10 and add the next root value into the number, this way we can generate the numbers on the fly.
+
+A recursion from root to bottom would look like 4 -> 4*10 + 9 -> 49*10 + 5 -> 495, return this value once we reach the leaf node.
+*/
+
 class Solution {
     public int sumNumbers(TreeNode root) {
 
